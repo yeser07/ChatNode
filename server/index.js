852f9044d2f43941738ba9eb4897c5e7ebd5d44c
yeser07 <yeser07@gmail.com>
2024-02-04 19:01:56 -0600

    Configurando cliente y server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,35 @@
 import express from "express";
 import logger from 'morgan';
 
+import {Server} from 'socket.io';
+import { createServer } from 'node:http';
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
+
+//CREAR SERVER CON CARATERISTICAS DE SOCKET.IO
+const server = createServer(app);
+const io = new Server(server);
+ 
+io.on('connection', ( socket)=> {
+    console.log('a user has connected!');
+    socket.on('disconnect', ()=>{
+        console.log('an user has disconneted');
+    })
+
+    socket.on('chat message', (msg)=>{
+        io.emit('chat message',msg);
+    })
+
+});
+
 app.use(logger('dev'));
 
 app.get('/',(req,res)=>{
     res.sendFile(process.cwd() + '/client/index.html');
 });
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
